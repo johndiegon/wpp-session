c955ec1c38a168843937fbfcaef1c0e628e449b1
Everton Gil Matos dos Santos <everton.santos@dotzmkt.com.br>
2022-02-14 18:32:10 -0300

    Correções no servidor para conseguir usar o socket.io

--- app.js
+++ app.js
@@ -1,113 +1,82 @@
-const { Client, MessageMedia } = require('whatsapp-web.js');
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const socketIO = require('socket.io');
+const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
-const http = require('http');
-const fs = require('fs');
-const { phoneNumberFormatter } = require('./helpers/formatter');
-const fileUpload = require('express-fileupload');
-const axios = require('axios');
-const mime = require('mime-types');
+const { Server } = require("socket.io");
 
-const port = process.env.PORT || 8000;
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-
-
-app.use(fileUpload({
-  debug: true
-}));
-
-
-// const SESSION_FILE_PATH = './whatsapp-session.json';
-let sessionCfg;
-
-// if (fs.existsSync(SESSION_FILE_PATH)) {
-//   sessionCfg = require(SESSION_FILE_PATH);
-// }
-
-const client = new Client({
-    restartOnAuthFail: true,
-    puppeteer: {
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process', // <- this one doesn't works in Windows
-        '--disable-gpu'
-      ],
-    },
-    session: sessionCfg
-  });
-
-  client.initialize();
-
-  
-// Socket IO
-io.on('connection', function(socket) {
-    socket.emit('message', 'Connecting...');
-  
-    client.on('qr', (qr) => {
-      console.log('QR RECEIVED', qr);
-      qrcode.toDataURL(qr, (err, url) => {
-        socket.emit('qr', url);
-        socket.emit('message', 'QR Code received, scan please!');
-      });
-    });
-  
-    client.on('ready', () => {
-      socket.emit('ready', 'Whatsapp is ready!');
-      socket.emit('message', 'Whatsapp is ready!');
-    });
-  
-    client.on('authenticated', (session) => {
-      socket.emit('authenticated', 'Whatsapp is authenticated!');
-      socket.emit('message', 'Whatsapp is authenticated!');
-      socket.emit('session', session);
-      console.log('AUTHENTICATED', session);
-      sessionCfg = session;
-      // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
-      //   if (err) {
-      //     console.error(err);
-      //   }
-      // });
-    });
-  
-    client.on('auth_failure', function(session) {
-      socket.emit('message', 'Auth failure, restarting...');
-    });
-  
-    // client.on('disconnected', (reason) => {
-    //   socket.emit('message', 'Whatsapp is disconnected!');
-    //   // fs.unlinkSync(SESSION_FILE_PATH, function(err) {
-    //   //     if(err) return console.log(err);
-    //   //     console.log('Session file deleted!');
-    //   // });
-    //   client.destroy();
-    //   client.initialize();
-    // });
-  });
-  
-
-  app.get('/', (req, res) => {
-    res.sendFile('index.html', {
-      root: __dirname
-    });
-  });
-
-  
-server.listen(port, function() {
-  console.log('App running on *: ' + port);
+const io = new Server('3000', {
+  cors: {
+    origin: '*'
+  }
 });
+
+let sessionCfg;
+const clientWhatsApp = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  session: sessionCfg
+});
+
+// Socket IO
+io.on('connection', function (socket) {
+  console.log('Conectou');
+
+  socket.emit('message', 'Conctando...');
+
+  socket.on('initialize', () => {
+    clientWhatsApp.initialize();
+    socket.emit('message', 'Abrindo Whats...');
+
+    
+    registerMessagesSocketWhats(socket);
+  });
+    
+  socket.on('disconnect', () => {
+    console.log('Desconectado');
+    clientWhatsApp.destroy();
+  });
+});
+
+
+
+const registerMessagesSocketWhats = (socket) => {
+  clientWhatsApp.on('qr', (qr) => {
+    // console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+  clientWhatsApp.on('ready', () => {
+    socket.emit('ready');
+    socket.emit('message', 'Whatsapp is ready!');
+  });
+
+  clientWhatsApp.on('authenticated', (session) => {
+    socket.emit('message', 'Whatsapp is authenticated!');
+    socket.emit('session', session);
+  });
+
+  clientWhatsApp.on('auth_failure', function (session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  clientWhatsApp.on('disconnected', (reason) => {
+    console.log('finalisou');
+    socket.emit('message', 'Whatsapp is disconnected!');
+    socket.emit('disconnectedWhats');
+    clientWhatsApp.destroy();
+    clientWhatsApp.initialize();
+  });
+}
